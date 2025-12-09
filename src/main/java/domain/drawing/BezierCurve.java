9508464396f59ff964eaac647cfdc45203ab1839
Skeletovich <dinocolakovic2@gmail.com>
2025-12-09 15:43:41 +0100

    Bezierova krivulja ažurirana da prima neograničen broj kontrolnih tačaka

--- src/main/java/domain/drawing/BezierCurve.java
+++ src/main/java/domain/drawing/BezierCurve.java
@@ -1,33 +1,29 @@
 package domain.drawing;
 
-public record BezierCurve(Point p0, Point p1, Point p2, Point p3) {
+import java.util.List;
 
+public record BezierCurve(List<Point> controlPoints) {
 
     public Point evaluate(double t) {
-        double x = Math.pow(1 - t, 3) * p0.x()
-                + 3 * Math.pow(1 - t, 2) * t * p1.x()
-                + 3 * (1 - t) * Math.pow(t, 2) * p2.x()
-                + Math.pow(t, 3) * p3.x();
+        int n = controlPoints.size() - 1;
+        double x = 0.0;
+        double y = 0.0;
 
-        double y = Math.pow(1 - t, 3) * p0.y()
-                + 3 * Math.pow(1 - t, 2) * t * p1.y()
-                + 3 * (1 - t) * Math.pow(t, 2) * p2.y()
-                + Math.pow(t, 3) * p3.y();
+        for (int i = 0; i <= n; i++) {
+            double binomial = binomialCoefficient(n, i);
+            double basis = binomial * Math.pow(1 - t, n - i) * Math.pow(t, i);
+            x += basis * controlPoints.get(i).x();
+            y += basis * controlPoints.get(i).y();
+        }
 
         return new Point(x, y);
     }
 
-
-    public double approximateLength(int samples) {
-        double length = 0.0;
-        Point prev = evaluate(0.0);
-        for (int i = 1; i <= samples; i++) {
-            double t = i / (double) samples;
-            Point curr = evaluate(t);
-            length += Math.sqrt(Math.pow(curr.x() - prev.x(), 2) +
-                    Math.pow(curr.y() - prev.y(), 2));
-            prev = curr;
+    private long binomialCoefficient(int n, int k) {
+        long res = 1;
+        for (int i = 0; i < k; i++) {
+            res = res * (n - i) / (i + 1);
         }
-        return length;
+        return res;
     }
 }
